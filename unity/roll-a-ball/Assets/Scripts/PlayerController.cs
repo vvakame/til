3f222f8311732e5d1d2783884bf2221816bab68a
vvakame <vvakame+dev@gmail.com>
2016-02-22 01:38:08 +0900

    Unity 5 - Roll a Ball game - 7 of 8: Displaying Score and Text - Unity Official Tutorials https://www.youtube.com/watch?v=bFSLI2cmYYo

--- unity/roll-a-ball/Assets/Scripts/PlayerController.cs
+++ unity/roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate()
@@ -26,6 +33,16 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (other.gameObject.CompareTag("Pick Up") ){
 			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win!";
 		}
 	}
 }
