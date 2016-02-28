a6fea58b74d8553c7b2589bdae81eed03f05cbf1
vvakame <vvakame@gmail.com>
2016-02-28 21:23:23 +0900

    触ったら色が変わる球を作った

---
+++ unity/leapmotion/Assets/Scripts/MarkerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("MarkerController#Start");
+	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        gameObject.GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name);
+        gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
