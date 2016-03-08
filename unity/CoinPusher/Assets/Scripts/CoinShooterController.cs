9891e3e1eb90e9fd03d5e5879d2b0d2f9db53616
vvakame <vvakame+dev@gmail.com>
2016-03-09 02:47:52 +0900

    CoinSpawnerを分解して、切り出したCoinShooterにはAxisにより射出方向を変更できる機能をつけた

---
+++ unity/CoinPusher/Assets/Scripts/CoinShooterController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinShooterController : MonoBehaviour
+{
+
+	public GameObject coinPrefab;
+
+	public int shootPiece = 1;
+	public float shootX = 0f;
+	public float shootY = 100f;
+	public float shootZ = 300f;
+	public float shootCourseRandomness = 10f;
+
+	public float moveSpeed = 0.8f;
+
+	private Quaternion defaultRotate;
+
+	void Start ()
+	{
+		defaultRotate = transform.rotation;
+	}
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0) || Input.GetButtonDown ("Jump")) {
+			for (var i = 0; i < shootPiece; i++) {
+				var coin = Instantiate (coinPrefab, transform.position, transform.rotation) as GameObject;
+				var coinRigid = coin.GetComponent<Rigidbody> ();
+				var shootForce = new Vector3 (shootX, shootY, shootZ);
+				coinRigid.AddRelativeForce (shootForce);
+				coin.transform.rotation = Random.rotation;
+			}
+			Score.Unlock ();
+		}
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		if (moveHorizontal < 0) {
+			transform.Rotate (Vector3.down * moveSpeed);
+		} else if (0 < moveHorizontal) {
+			transform.Rotate (Vector3.up * moveSpeed);
+		}
+	}
+}
--- unity/CoinPusher/Assets/Scripts/CoinSpawnerController.cs
+++ unity/CoinPusher/Assets/Scripts/CoinSpawnerController.cs
@@ -9,30 +9,10 @@ public class CoinSpawnerController : MonoBehaviour
 	public int startSpawn = 100;
 	public float startSpawnScale = 0.8f;
 
-	public int shootPiece = 1;
-	public float shootX = 0f;
-	public float shootY = 100f;
-	public float shootZ = 300f;
-	public float shootCourseRandomness = 10f;
-
 	void Start ()
 	{
 		for (var i = 0; i < startSpawn; i++) {
 			Instantiate (coinPrefab, transform.position + new Vector3 ((Random.value - 0.5f) * startSpawnScale, 0, ((Random.value - 1.0f) * 0.5f) * startSpawnScale), transform.rotation);
-		}
-	}
-
-	void Update ()
-	{
-		if (Input.GetMouseButtonDown (0) || Input.GetButtonDown ("Jump")) {
-			for (var i = 0; i < shootPiece; i++) {
-				var coin = Instantiate (coinPrefab, transform.position, transform.rotation) as GameObject;
-				var coinRigid = coin.GetComponent<Rigidbody> ();
-				var shootForce = new Vector3 (shootX, shootY, shootZ);
-				coinRigid.AddRelativeForce (shootForce);
-				coin.transform.rotation = Random.rotation;
-			}
-			Score.Unlock ();
 		}
 	}
 }
