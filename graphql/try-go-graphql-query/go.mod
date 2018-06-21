f5cb2439105d62c3117a5989c5c0a88218ac9e70
vvakame <vvakame+dev@gmail.com>
2018-06-21 16:52:02 +0900

    実際にGitHubのエンドポイント叩いてみた

--- graphql/try-go-graphql-query/go.mod
+++ graphql/try-go-graphql-query/go.mod
@@ -1,3 +1,8 @@
 module github.com/vvakame/til/graphql/try-go-graphql-query
 
-require github.com/motemen/go-graphql-query v0.0.0-20170605235615-01c39f52ab0b
+require (
+	github.com/k0kubun/pp v1.3.0
+	github.com/mattn/go-colorable v0.0.9
+	github.com/mattn/go-isatty v0.0.3
+	github.com/motemen/go-graphql-query v0.0.0-20170605235615-01c39f52ab0b
+)
--- graphql/try-go-graphql-query/main.go
+++ graphql/try-go-graphql-query/main.go
@@ -1,55 +1,109 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 
+	"github.com/k0kubun/pp"
 	"github.com/motemen/go-graphql-query"
 )
 
 func main() {
-	type Edge struct {
-		Cursor string
+	type Query struct {
+		User *struct {
+			GraphQLArguments struct {
+				Login string `graphql:"$login,notnull"`
+			}
+			Name          string
+			Organizations *struct {
+				GraphQLArguments struct {
+					First int `graphql:"$organizationFirst,notnull"`
+				}
+				Edges []*struct {
+					Cursor string // Edge みたいなembed structの展開は思い通りじゃなかった
+					Node   *struct {
+						Name string
+					}
+				}
+				Nodes []*struct {
+					Name string
+				}
+			} `graphql:"(first: $organizationFirst)"`
+		} `graphql:"(login: $login)"`
+
+		TilRepository *struct {
+			GraphQLArguments struct {
+				Owner string `graphql:"$tilOwner,notnull"`
+				Name  string `graphql:"$tilRepo,notnull"`
+			}
+			Name string
+		} `graphql:"alias=repository,(owner: $tilOwner, name: $tilRepo)"`
+
+		TsfmtRepository *struct {
+			GraphQLArguments struct {
+				Owner string `graphql:"$tsfmtOwner,notnull"`
+				Name  string `graphql:"$tsfmtRepo,notnull"`
+			}
+			Name string
+		} `graphql:"alias=repository,(owner: $tsfmtOwner, name: $tsfmtRepo)"`
 	}
 
-	type Organization struct {
-		Name string
-	}
-
-	type OrganizationEdge struct {
-		Cursor string // Edge みたいなembed structの展開は思い通りじゃなかった
-		Node   *Organization
-	}
-
-	type OrganizationConnection struct {
-		Edges []*OrganizationEdge
-		Nodes []*Organization
-	}
-
-	type User struct {
-		Name          string
-		Organizations []*OrganizationConnection `graphql:"(first: $organizationFirst)"`
-	}
-
-	type Repository struct {
-		Name string
-	}
-
-	type Test struct {
-		GraphQLArguments struct {
-			User              string `graphql:"$login,notnull"`
-			OrganizationFirst int    `graphql:"$organizationFirst,notnull"`
-		}
-		User            *User       `graphql:"(login: $login)"`
-		TilRepository   *Repository `graphql:"alias=repository,(owner: \"vvakame\", name: \"til\")"`
-		TsfmtRepository *Repository `graphql:"alias=repository,(owner: \"vvakame\", name: \"typescript-formatter\")"`
-	}
-
-	obj := &Test{}
+	obj := &Query{}
 
 	query, err := graphqlquery.Build(obj)
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Println(string(query))
+
+	req := &struct {
+		Query     string                 `json:"query"`
+		Variables map[string]interface{} `json:"variables"`
+	}{
+		Query: string(query),
+		Variables: map[string]interface{}{
+			"login":             "vvakame",
+			"organizationFirst": 100,
+			"tilOwner":          "vvakame",
+			"tilRepo":           "til",
+			"tsfmtOwner":        "vvakame",
+			"tsfmtRepo":         "typescript-formatter",
+		},
+	}
+	reqB, err := json.MarshalIndent(req, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(reqB))
+
+	apiReq, err := http.NewRequest("POST", "https://api.github.com/graphql", bytes.NewReader(reqB))
+	if err != nil {
+		panic(err)
+	}
+	apiReq.Header.Add("Content-Type", "application/json")
+	apiReq.Header.Add("Authorization", "bearer "+os.Getenv("GITHUB_TOKEN"))
+	resp, err := http.DefaultClient.Do(apiReq)
+	if err != nil {
+		panic(err)
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
+
+	type Resp struct {
+		Data *Query `json:"data"`
+	}
+	err = json.Unmarshal(b, &Resp{obj})
+	if err != nil {
+		panic(err)
+	}
+
+	pp.Println(obj)
 }
