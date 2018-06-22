252107ec8537d9ffb41875fb8c759b17fb142c17
vvakame <vvakame+dev@gmail.com>
2018-06-22 19:12:57 +0900

    handler.RecoverFunc と handler.ResolverMiddleware と handler.RequestMiddleware を試した

--- graphql/try-go-gqlgen/main.go
+++ graphql/try-go-gqlgen/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
-
-	"context"
+	"os"
+	"runtime/debug"
 
 	"github.com/pkg/errors"
 	"github.com/vektah/gqlgen/graphql"
@@ -17,6 +18,7 @@ import (
 func main() {
 	app := graph.NewMyApp()
 	http.Handle("/", handler.Playground("Todo", "/query"))
+
 	http.Handle("/query",
 		models.DataloaderMiddleware(
 			app.UserMap,
@@ -34,6 +36,24 @@ func main() {
 						},
 						"foobar!",
 					}
+				}),
+				handler.RecoverFunc(func(ctx context.Context, err interface{}) error {
+					// panicからの回復方法
+					fmt.Fprintln(os.Stderr, err)
+					fmt.Fprintln(os.Stderr)
+					debug.PrintStack()
+
+					return nil
+				}),
+				handler.ResolverMiddleware(func(ctx context.Context, next graphql.Resolver) (interface{}, error) {
+					// Resolverが1回仕事することになるたびに呼ばれるようだ
+					res, err := next(ctx)
+					return res, err
+				}),
+				handler.RequestMiddleware(func(ctx context.Context, next func(ctx context.Context) []byte) []byte {
+					// 結果のJSONの "data" のvalue部分のbyte列が帰ってくるようだ
+					b := next(ctx)
+					return b
 				}),
 			),
 		),
