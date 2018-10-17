6376af4d6b0c662fdaedbd45eac050f38d1b6f89
vvakame <vvakame+dev@gmail.com>
2018-10-17 13:39:59 +0900

    try zap logger with github.com/blendle/zapdriver

--- appengine/go111-sample/main.go
+++ appengine/go111-sample/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	rlog "log"
+	"log"
 	"net/http"
 	"os"
 	"os/signal"
@@ -16,38 +16,48 @@ import (
 	"cloud.google.com/go/cloudtasks/apiv2beta3"
 	"contrib.go.opencensus.io/exporter/stackdriver"
 	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
+	"github.com/blendle/zapdriver"
 	"github.com/favclip/ucon"
-	"github.com/vvakame/til/appengine/go111-sample/log"
 	"go.mercari.io/datastore"
 	"go.mercari.io/datastore/boom"
 	"go.mercari.io/datastore/clouddatastore"
 	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/trace"
+	"go.uber.org/zap"
 	taskspb "google.golang.org/genproto/googleapis/cloud/tasks/v2beta3"
 )
 
 var dsClient datastore.Client
+var logger *zap.Logger
 
 func main() {
 
-	close, err := log.Init()
-	if err != nil {
-		rlog.Fatalf("Failed to create logger: %v", err)
+	var err error
+
+	if os.Getenv("LOCAL_EXEC") == "" {
+		config := zapdriver.NewProductionConfig()
+		logger, err = config.Build()
+		if err != nil {
+			log.Fatalf("Failed to create zap logger: %v", err)
+		}
+
+	} else {
+		logger = zap.NewNop()
 	}
-	defer close()
+	defer logger.Sync()
 
 	exporter, err := stackdriver.NewExporter(stackdriver.Options{
 		ProjectID: os.Getenv("GOOGLE_CLOUD_PROJECT"),
 	})
 	if err != nil {
-		rlog.Fatalf("Failed to create stackdriver exporter: %v", err)
+		log.Fatalf("Failed to create stackdriver exporter: %v", err)
 	}
 	trace.RegisterExporter(exporter)
 	defer exporter.Flush()
 
 	dsClient, err = clouddatastore.FromContext(context.Background())
 	if err != nil {
-		rlog.Fatalf("Failed to create cloud datastore client: %v", err)
+		log.Fatalf("Failed to create cloud datastore client: %v", err)
 	}
 	defer dsClient.Close()
 
@@ -56,10 +66,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
-		rlog.Printf("Defaulting to port %s", port)
+		log.Printf("Defaulting to port %s", port)
 	}
 
-	rlog.Printf("Listening on port %s", port)
+	log.Printf("Listening on port %s", port)
 
 	server := &http.Server{
 		Addr: fmt.Sprintf(":%s", port),
@@ -71,11 +81,11 @@ func main() {
 
 	go func() {
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			rlog.Fatal(err)
+			log.Fatal(err)
 		}
 	}()
 
-	rlog.Printf("running...")
+	log.Printf("running...")
 
 	// setup graceful shutdown...
 	sigCh := make(chan os.Signal, 1)
@@ -84,29 +94,22 @@ func main() {
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	if err := server.Shutdown(ctx); err != nil {
-		rlog.Fatalf("graceful shutdown failure: %s", err)
+		log.Fatalf("graceful shutdown failure: %s", err)
 	}
-	rlog.Printf("graceful shutdown successfully")
+	log.Printf("graceful shutdown successfully")
 }
 
 func handlerMain() {
-	ucon.Middleware(func(b *ucon.Bubble) error {
-		b.Context = log.WithContext(b.Context, b.R)
-		b.R = b.R.WithContext(b.Context)
-		return b.Next()
-	})
 	ucon.Orthodox()
 
 	// https://cloud.google.com/appengine/docs/standard/go111/how-instances-are-managed#instance_scaling
 	// Automatic scaling の時は動かないはず
 	ucon.HandleFunc("*", "/_ah/start", func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		log.Infof(ctx, "on /_ah/start")
+		logger.Info("on /_ah/start")
 		fmt.Fprint(w, "on start!")
 	})
 	ucon.HandleFunc("*", "/_ah/stop", func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-		log.Infof(ctx, "on /_ah/stop")
+		logger.Info("on /_ah/stop")
 		fmt.Fprint(w, "on stop!")
 	})
 
@@ -130,8 +133,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	ctx, span := trace.StartSpan(ctx, "indexHandler")
 	defer span.End()
 
-	log.Debugf(ctx, "Hi, 1")
-	log.Infof(ctx, "Hi, 2")
+	logger.Debug("Hi, 1")
+	logger.Info("Hi, 2")
 
 	fmt.Fprint(w, "Hello, World!")
 }
@@ -167,7 +170,7 @@ func fibonacciHandler(w http.ResponseWriter, r *http.Request) error {
 		defer span.End()
 
 		v := f()
-		log.Debugf(ctx, "#%d: %d", i, v)
+		logger.Debug("loop", zap.Int("index", i), zap.Int("value", v))
 		fmt.Fprintf(w, "%d\n", v)
 	}
 
@@ -197,7 +200,7 @@ func datastoreHandler(w http.ResponseWriter, r *http.Request) error {
 	go func() {
 		key, err := bm.Put(&Go111SampleKind{})
 		if err != nil {
-			rlog.Fatal(err)
+			log.Fatal(err)
 		}
 		fmt.Fprintf(w, "%s\n", key.String())
 		wg.Done()
@@ -205,7 +208,7 @@ func datastoreHandler(w http.ResponseWriter, r *http.Request) error {
 	go func() {
 		key, err := bm.Put(&Go111SampleKind{})
 		if err != nil {
-			rlog.Fatal(err)
+			log.Fatal(err)
 		}
 		fmt.Fprintf(w, "%s\n", key.String())
 		wg.Done()
@@ -213,7 +216,7 @@ func datastoreHandler(w http.ResponseWriter, r *http.Request) error {
 	go func() {
 		key, err := bm.Put(&Go111SampleKind{})
 		if err != nil {
-			rlog.Fatal(err)
+			log.Fatal(err)
 		}
 		fmt.Fprintf(w, "%s\n", key.String())
 		wg.Done()
@@ -249,7 +252,7 @@ func cloudTasksHandler(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		return err
 	}
-	log.Infof(ctx, "on taskClient.CreateTask: %+v", task)
+	logger.Info("on taskClient.CreateTask", zap.Any("task", task))
 
 	fmt.Fprintf(w, "added!")
 
@@ -258,9 +261,7 @@ func cloudTasksHandler(w http.ResponseWriter, r *http.Request) error {
 
 func cloudTaskExecHandler(w http.ResponseWriter, r *http.Request) error {
 
-	ctx := r.Context()
-
-	log.Infof(ctx, "task done!")
+	logger.Info("task done!")
 	fmt.Fprintf(w, "done!")
 
 	return nil
