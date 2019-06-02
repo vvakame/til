1242471710ea1ffb99606539519db80e0446226e
vvakame <vvakame+dev@gmail.com>
2019-06-03 00:56:30 +0900

    ざっくり何か出るようにした

--- github-actions/md-to-blogpost/go.mod
+++ github-actions/md-to-blogpost/go.mod
@@ -5,8 +5,7 @@ go 1.12
 require (
 	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
 	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
-	github.com/shurcooL/githubv4 v0.0.0-20190119021625-d9689b595017
-	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
+	github.com/google/go-github/v25 v25.1.1
 	github.com/stretchr/testify v1.3.0 // indirect
 	golang.org/x/oauth2 v0.0.0-20190523182746-aaccbc9213b0
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
--- github-actions/md-to-blogpost/main.go
+++ github-actions/md-to-blogpost/main.go
@@ -3,17 +3,23 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/md5"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"html/template"
-	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"path"
+	"regexp"
+	"strings"
 	"time"
 
-	"github.com/shurcooL/githubv4"
+	"github.com/google/go-github/v25/github"
 	"golang.org/x/oauth2"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -23,11 +29,22 @@ var (
 
 	githubEventPath = kingpin.Flag("github_event_path", "GitHub event data json path.").Default(os.Getenv("GITHUB_EVENT_PATH")).String()
 
-	owner        = kingpin.Flag("owner", "name of repository owner.").String()
-	name         = kingpin.Flag("name", "name of repository.").String()
-	prNumber     = kingpin.Flag("pr_number", "number of pull request.").Int()
-	templatePath = kingpin.Flag("template_path", "markdown template path. it uses with html/template").String()
-	outputPath   = kingpin.Arg("output_path", "result output path").Default("result.md").String()
+	owner = kingpin.Flag("owner", "name of repository owner.").String()
+	name  = kingpin.Flag("name", "name of repository.").Required().String()
+
+	baseBranch = kingpin.Flag("base_branch", "name of base branch.").Default("master").String()
+
+	commitMessage = kingpin.Flag("commit_message", "commit message text.").Default("auto generated commit").String()
+	timezone      = kingpin.Flag("timezone", "timezone for blog post date").Default("UTC").String()
+	postPath      = kingpin.Flag("post_path", "blog post markdown path in repository.").String()
+	imagePath     = kingpin.Flag("image_path", "blog post image path in repository.").String()
+	baseImageURL  = kingpin.Flag("base_image_url", "base image url in blog site.").Default("/images").String()
+
+	prBranch = kingpin.Flag("pr_branch", "name of pull request branch.").String()
+	prTitle  = kingpin.Flag("pr_title", "title of pull request.").String()
+	prBody   = kingpin.Flag("pr_body", "body of pull request.").String()
+
+	inputPath = kingpin.Arg("input_path", "input markdown path").Required().String()
 )
 
 func main() {
@@ -57,155 +74,318 @@ func main() {
 	if *owner == "" {
 		*owner = eventData.Repository.Owner.Login
 	}
-	if *name == "" {
-		*name = eventData.Repository.Name
+	if *postPath == "" {
+		loc, err := time.LoadLocation(*timezone)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var postDate string
+		if eventData.PullRequest.MergedAt.IsZero() {
+			postDate = eventData.PullRequest.MergedAt.In(loc).Format("2006-01-02")
+		} else {
+			postDate = time.Now().In(loc).Format("2006-01-02")
+		}
+		*postPath = fmt.Sprintf("source/_posts/%s-%s.md", postDate, eventData.PullRequest.Head.Ref)
 	}
-	if *prNumber == 0 {
-		*prNumber = eventData.Number
+	if *imagePath == "" {
+		loc, err := time.LoadLocation(*timezone)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var postDate string
+		if eventData.PullRequest.MergedAt.IsZero() {
+			postDate = eventData.PullRequest.MergedAt.In(loc).Format("2006-01-02")
+		} else {
+			postDate = time.Now().In(loc).Format("2006-01-02")
+		}
+		*imagePath = fmt.Sprintf("source/images/%s-%s", postDate, eventData.PullRequest.Head.Ref)
+	}
+	if *prBranch == "" {
+		*prBranch = fmt.Sprintf("from-pr-%d", eventData.Number)
+	}
+	if *prTitle == "" {
+		*prTitle = fmt.Sprintf("blog post from '%s'", eventData.PullRequest.Title)
+	}
+	if *prBody == "" {
+		*prBody = fmt.Sprintf("from %s", eventData.PullRequest.HTMLURL)
 	}
 
-	src := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: *githubToken})
-	httpClient := oauth2.NewClient(ctx, src)
+	client := github.NewClient(
+		oauth2.NewClient(
+			ctx,
+			oauth2.StaticTokenSource(&oauth2.Token{AccessToken: *githubToken}),
+		),
+	)
 
-	client := githubv4.NewClient(httpClient)
-	resp, err := getPRInfo(ctx, client, *owner, *name, *prNumber)
+	contentMap, err := createContent(ctx, &CreateContentReq{
+		InputPath:    *inputPath,
+		PostPath:     *postPath,
+		ImagePath:    *imagePath,
+		BaseImageURL: *baseImageURL,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	f, err := os.OpenFile(*outputPath, os.O_WRONLY|os.O_CREATE, 0666)
+	branchRef, commit, err := createNewBranch(ctx, &CreateCommitReq{
+		Client: client,
+		Owner:  *owner,
+		Name:   *name,
+
+		BaseRef:   fmt.Sprintf("refs/heads/%s", *baseBranch),
+		BranchRef: fmt.Sprintf("refs/heads/%s", *prBranch),
+
+		Message: *commitMessage,
+		Files:   contentMap,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = generateMarkdown(ctx, f, *templatePath, resp)
+	log.Println(branchRef.GetURL())
+	log.Println(commit.GetURL())
+
+	pr, err := createPR(ctx, &CreatePRReq{
+		Client: client,
+		Owner:  *owner,
+		Name:   *name,
+		Title:  *prTitle,
+		Body:   *prBody,
+		Base:   *baseBranch,
+		Head:   *prBranch,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println(pr.GetURL())
+}
+
+type CreateContentReq struct {
+	InputPath string
+
+	PostPath     string
+	ImagePath    string
+	BaseImageURL string
+}
+
+func createContent(ctx context.Context, req *CreateContentReq) (map[string][]byte, error) {
+
+	inputPath := req.InputPath
+	postPath := req.PostPath
+	imagePath := req.ImagePath
+	baseImageURL := req.BaseImageURL
+
+	b, err := ioutil.ReadFile(inputPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// NOTE
+	//   AST → markdown できるいい感じのライブラリないかな…
+	//   🙅 https://github.com/russross/blackfriday
+	//   🙅 https://godoc.org/github.com/gomarkdown/markdown
+
+	re := regexp.MustCompile(`!\[(?P<alt>[^]]*)]\((?P<url>[^)]+)\)`)
+
+	getContent := func(imageURL string) ([]byte, error) {
+		parsed, err := url.Parse(imageURL)
+		if err != nil {
+			return nil, err
+		}
+
+		if parsed.Host == "" {
+			return ioutil.ReadFile(imageURL)
+		}
+
+		resp, err := http.Get(imageURL)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode < 200 && 300 <= resp.StatusCode {
+			return nil, fmt.Errorf("fetch image failed: %d %s", resp.StatusCode, imageURL)
+		}
+		return ioutil.ReadAll(resp.Body)
+	}
+
+	contentMap := make(map[string][]byte)
+
+	var buf bytes.Buffer
+	tail := 0
+	for _, submatches := range re.FindAllSubmatchIndex(b, -1) {
+		alt := string(b[submatches[2]:submatches[3]])
+		imageURL := string(b[submatches[4]:submatches[5]])
+
+		imageBlob, err := getContent(imageURL)
+		if err != nil {
+			return nil, err
+		}
+		mimeType := http.DetectContentType(imageBlob)
+		ext := strings.TrimPrefix(mimeType, "image/")
+		fileName := fmt.Sprintf("%x.%s", md5.Sum(imageBlob), ext)
+
+		// before matching
+		buf.Write(b[tail:submatches[0]])
+		// ![
+		buf.Write(b[submatches[0]:submatches[2]])
+		// alt
+		buf.WriteString(alt)
+		// ](
+		buf.Write(b[submatches[3]:submatches[4]])
+		// imageURL
+		buf.WriteString(path.Join(baseImageURL, fileName))
+		// )
+		buf.Write(b[submatches[5]:submatches[1]])
+
+		tail = submatches[1]
+
+		contentMap[path.Join(imagePath, fileName)] = imageBlob
+	}
+	buf.Write(b[tail:])
+
+	contentMap[postPath] = buf.Bytes()
+
+	return contentMap, nil
+}
+
+type CreateCommitReq struct {
+	Client *github.Client
+	Owner  string
+	Name   string
+
+	BranchRef string
+
+	Message string
+	BaseRef string
+	Files   map[string][]byte
+}
+
+func createNewBranch(ctx context.Context, req *CreateCommitReq) (*github.Reference, *github.Commit, error) {
+
+	client := req.Client
+	owner := req.Owner
+	name := req.Name
+
+	baseRef := req.BaseRef
+	if baseRef == "" {
+		baseRef = "refs/heads/master"
+	}
+
+	stringPtr := func(s string) *string {
+		return &s
+	}
+
+	ref, _, err := client.Git.GetRef(ctx, owner, name, baseRef)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	commit, _, err := client.Git.GetCommit(ctx, owner, name, ref.GetObject().GetSHA())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var entries []github.TreeEntry
+	for filePath, content := range req.Files {
+		blob, _, err := client.Git.CreateBlob(ctx, owner, name, &github.Blob{
+			Encoding: stringPtr("base64"),
+			Content:  stringPtr(base64.StdEncoding.EncodeToString(content)),
+		})
+		if err != nil {
+			return nil, nil, err
+		}
+
+		entries = append(entries, github.TreeEntry{
+			Path: stringPtr(filePath),
+			Mode: stringPtr("100644"),
+			Type: stringPtr("blob"),
+			SHA:  stringPtr(blob.GetSHA()),
+		})
+	}
+
+	tree, _, err := client.Git.CreateTree(ctx, owner, name, commit.GetTree().GetSHA(), entries)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	newCommit, _, err := client.Git.CreateCommit(ctx, owner, name, &github.Commit{
+		Message: &req.Message,
+		Parents: []github.Commit{*commit},
+		Tree:    tree,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	branchRef, _, err := client.Git.CreateRef(ctx, owner, name, &github.Reference{
+		Ref: stringPtr(req.BranchRef),
+		Object: &github.GitObject{
+			SHA: stringPtr(newCommit.GetSHA()),
+		},
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return branchRef, newCommit, nil
+}
+
+type CreatePRReq struct {
+	Client *github.Client
+	Owner  string
+	Name   string
+
+	Title string
+	Body  string
+	Base  string
+	Head  string
+}
+
+func createPR(ctx context.Context, req *CreatePRReq) (*github.PullRequest, error) {
+
+	client := req.Client
+	owner := req.Owner
+	name := req.Name
+
+	stringPtr := func(s string) *string {
+		return &s
+	}
+
+	if req.Base == "" {
+		req.Base = "master"
+	}
+
+	maintainerCanModify := true
+
+	pr, _, err := client.PullRequests.Create(ctx, owner, name, &github.NewPullRequest{
+		Title:               stringPtr(req.Title),
+		Head:                stringPtr(req.Head),
+		Base:                stringPtr(req.Base),
+		Body:                stringPtr(req.Body),
+		MaintainerCanModify: &maintainerCanModify,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return pr, nil
 }
 
 type GitHubEvent struct {
-	Number     int
+	Number      int
+	PullRequest struct {
+		Title    string
+		HTMLURL  string    `json:"html_url"`
+		MergedAt time.Time `json:"merged_at"`
+		Head     struct {
+			Ref string
+		}
+	} `json:"pull_request"`
 	Repository struct {
 		Name  string
 		Owner struct {
 			Login string
 		}
 	}
-}
-
-func generateMarkdown(ctx context.Context, w io.Writer, templatePath string, resp *PRInfo) error {
-
-	var templateText string
-	if templatePath == "" {
-		staticFS, err := fs.New()
-		if err != nil {
-			return err
-		}
-		b, err := fs.ReadFile(staticFS, "/markdown.tmpl.md")
-		if err != nil {
-			return err
-		}
-		templateText = string(b)
-	} else {
-		b, err := ioutil.ReadFile(templatePath)
-		if err != nil {
-			return err
-		}
-		templateText = string(b)
-	}
-
-	pr := resp.Repository.PullRequest
-
-	_, _ = fmt.Fprintf(w, "---\n")
-	err := yaml.NewEncoder(w).Encode(struct {
-		Title string   `yaml:"title"`
-		Date  string   `yaml:"date"`
-		Tags  []string `yaml:"tags"`
-	}{
-		Title: string(pr.Title),
-		Date:  time.Now().Format("2006-01-02 15:04:05"), // TODO Timezone
-		Tags:  []string{},                               // TODO
-	})
-	if err != nil {
-		return err
-	}
-	_, _ = fmt.Fprintf(w, "---\n\n")
-
-	samePrevStock := make(map[string]string)
-	tmpl, err := template.
-		New("pr2md").
-		Funcs(map[string]interface{}{
-			"date": func(t githubv4.DateTime) string {
-				return t.Format("2006-01-02 15:04:05") // TODO Timezone
-			},
-			"isSamePrev": func(group string, value githubv4.String) bool {
-				if samePrevStock[group] == string(value) {
-					return true
-				}
-				samePrevStock[group] = string(value)
-				return false
-			},
-		}).
-		Parse(templateText)
-	if err != nil {
-		return err
-	}
-
-	return tmpl.Execute(w, &pr)
-}
-
-type PRInfo struct {
-	Repository struct {
-		PullRequest struct {
-			Title  githubv4.String
-			URL    githubv4.String
-			Body   githubv4.String
-			Author struct {
-				Login     githubv4.String
-				AvatarURL githubv4.String `graphql:"avatarUrl(size: $avatarImageSize)"`
-			}
-			CreatedAt githubv4.DateTime
-			Files     struct {
-				PageInfo struct {
-					HasNextPage githubv4.Boolean
-					EndCursor   githubv4.String
-				}
-				TotalCount githubv4.Int
-				Nodes      []struct {
-					Path      githubv4.String
-					Additions githubv4.Int
-					Deletions githubv4.Int
-				}
-			} `graphql:"files(first: 100)"`
-			Comments struct {
-				PageInfo struct {
-					HasNextPage githubv4.Boolean
-					EndCursor   githubv4.String
-				}
-				TotalCount githubv4.Int
-				Nodes      []struct {
-					Author struct {
-						Login     githubv4.String
-						AvatarURL githubv4.String `graphql:"avatarUrl(size: $avatarImageSize)"`
-					}
-					Body      githubv4.String
-					CreatedAt githubv4.DateTime
-				}
-			} `graphql:"comments(first: 100)"`
-		} `graphql:"pullRequest(number: $prNumber)"`
-	} `graphql:"repository(owner: $owner, name: $name)"`
-}
-
-func getPRInfo(ctx context.Context, cli *githubv4.Client, owner, name string, prNumber int) (*PRInfo, error) {
-	var query PRInfo
-	variables := map[string]interface{}{
-		"owner":           githubv4.String(owner),
-		"name":            githubv4.String(name),
-		"prNumber":        githubv4.Int(prNumber),
-		"avatarImageSize": githubv4.Int(64),
-	}
-
-	err := cli.Query(ctx, &query, variables)
-	return &query, err
 }
