a3385ae623be2a80488b5b902d260b3ad11165c8
vvakame <vvakame+dev@gmail.com>
2019-06-03 12:00:18 +0900

    いつもの

--- graphql/fetch-github-activities/src/index.ts
+++ graphql/fetch-github-activities/src/index.ts
@@ -8,8 +8,8 @@ const ignoreOrgs = [
     "techbookfest",
     "TechBooster",
 ];
-const start = new Date("2019-05-18T09:00:00Z");
-const end = new Date("2019-05-25T09:00:00Z");
+const start = new Date("2019-05-25T09:00:00Z");
+const end = new Date("2019-06-01T09:00:00Z");
 
 // https://developer.github.com/v4/explorer/
 const query = `
