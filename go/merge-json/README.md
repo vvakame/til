10ae69c48cc6a337c54adde819c2916657c095f0
vvakame <vvakame+dev@gmail.com>
2021-08-14 15:59:38 +0900

    cでreflect系の処理をキャッシュする版

--- go/merge-json/README.md
+++ go/merge-json/README.md
@@ -27,3 +27,17 @@ BenchmarkMarshallers/c-8 	  708204	      1699 ns/op	    1368 B/op	      21 alloc
 PASS
 ok  	github.com/vvakame/til/go/merge-json	4.197s
 ```
+
+cでreflect系の処理をキャッシュする版
+
+```shell
+$ go test -bench . -benchmem
+goos: darwin
+goarch: arm64
+pkg: github.com/vvakame/til/go/merge-json
+BenchmarkMarshallers/a-8 	 3400077	       342.7 ns/op	      48 B/op	       2 allocs/op
+BenchmarkMarshallers/b-8 	  610495	      1962 ns/op	    1240 B/op	      35 allocs/op
+BenchmarkMarshallers/c-8 	 2389891	       488.6 ns/op	     144 B/op	       3 allocs/op
+PASS
+ok  	github.com/vvakame/til/go/merge-json	4.510s
+```
--- go/merge-json/c/marshal_json.go
+++ go/merge-json/c/marshal_json.go
@@ -4,7 +4,14 @@ import (
 	"encoding/json"
 	"errors"
 	"reflect"
+	"sync"
 )
+
+var m = &merger{
+	cacheValue: &cacheValue{
+		nextTypes: make(map[reflect.Type]*cacheValue),
+	},
+}
 
 func MarshalFlatten(objs ...interface{}) ([]byte, error) {
 	if len(objs) == 0 {
@@ -13,7 +20,7 @@ func MarshalFlatten(objs ...interface{}) ([]byte, error) {
 		return json.Marshal(objs[0])
 	}
 
-	merged, err := mergeFields(objs...)
+	merged, err := m.mergeObjects(objs...)
 	if err != nil {
 		return nil, err
 	}
@@ -21,40 +28,155 @@ func MarshalFlatten(objs ...interface{}) ([]byte, error) {
 	return json.Marshal(merged)
 }
 
-func mergeFields(objs ...interface{}) (interface{}, error) {
-	var sfs []reflect.StructField
-	var vs []reflect.Value
-	for _, obj := range objs {
-		v := reflect.ValueOf(obj)
-		if v.Kind() != reflect.Ptr {
-			return nil, errors.New("must be a pointer")
-		}
-		if v.IsNil() {
-			return nil, errors.New("must be a non-nil pointer")
-		}
-		v = v.Elem()
-		if v.Kind() != reflect.Struct {
-			return nil, errors.New("must be a pointer to struct")
-		}
+type merger struct {
+	*cacheValue
+}
 
-		for i := 0; i < v.NumField(); i++ {
-			sf := v.Type().Field(i)
-			v := v.Field(i)
-			if !v.CanSet() {
-				continue
-			}
+type cacheValue struct {
+	sync.RWMutex
+	nextTypes map[reflect.Type]*cacheValue
+	typeCache *typeCache
+}
 
-			sfs = append(sfs, sf)
-			vs = append(vs, v)
-		}
+type typeCache struct {
+	st  reflect.Type
+	vas []*valueAccessor
+}
+
+type valueAccessor struct {
+	skip bool
+
+	toFieldIndex int
+
+	objIndex   int
+	fieldIndex int
+}
+
+func (m *merger) mergeObjects(objs ...interface{}) (interface{}, error) {
+	typeCache, err := m.getTypeCache(objs, nil)
+	if err != nil {
+		return nil, err
 	}
 
-	st := reflect.StructOf(sfs)
+	return typeCache.mergeObjects(objs...)
+}
 
-	s := reflect.New(st).Elem()
-	for i := 0; i < len(vs); i++ {
-		s.Field(i).Set(vs[i])
+func (cv *cacheValue) getTypeCache(objs, rest []interface{}) (*typeCache, error) {
+	if len(objs) == 0 {
+		return nil, errors.New("objs len is 0")
+	}
+	if len(rest) == 0 {
+		rest = objs
+	}
+
+	obj := rest[0]
+	v, err := toBareStructValue(obj)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(rest) == 1 {
+		cv.RLock()
+		if cv.typeCache != nil {
+			cv.RUnlock()
+			return cv.typeCache, nil
+		}
+		cv.RUnlock()
+
+		cv.Lock()
+
+		var sfs []reflect.StructField
+		var vas []*valueAccessor
+		var currentLoop int
+		for objIdx, obj := range objs {
+			v, err := toBareStructValue(obj)
+			if err != nil {
+				return nil, err
+			}
+
+			for i := 0; i < v.NumField(); i++ {
+				sf := v.Type().Field(i)
+				v := v.Field(i)
+
+				if !v.CanSet() {
+					vas = append(vas, &valueAccessor{
+						skip: true,
+					})
+					continue
+				}
+
+				sfs = append(sfs, sf)
+				vas = append(vas, &valueAccessor{
+					toFieldIndex: currentLoop,
+					objIndex:     objIdx,
+					fieldIndex:   i,
+				})
+				currentLoop++
+			}
+		}
+
+		st := reflect.StructOf(sfs)
+
+		cv.typeCache = &typeCache{
+			st:  st,
+			vas: vas,
+		}
+		cv.Unlock()
+
+		return cv.typeCache, nil
+	}
+
+	cv.RLock()
+	v, err = toBareStructValue(rest[1])
+	if err != nil {
+		return nil, err
+	}
+	next, ok := cv.nextTypes[v.Type()]
+	if !ok {
+		cv.RUnlock()
+		cv.Lock()
+		next = &cacheValue{
+			nextTypes: make(map[reflect.Type]*cacheValue),
+		}
+		cv.nextTypes[v.Type()] = next
+		cv.Unlock()
+	}
+
+	return next.getTypeCache(objs, rest[1:])
+}
+
+func (tc *typeCache) mergeObjects(objs ...interface{}) (interface{}, error) {
+	s := reflect.New(tc.st).Elem()
+	for _, va := range tc.vas {
+		if va.skip {
+			continue
+		}
+
+		obj := objs[va.objIndex]
+
+		v, err := toBareStructValue(obj)
+		if err != nil {
+			return nil, err
+		}
+
+		s.Field(va.toFieldIndex).Set(v.Field(va.fieldIndex))
 	}
 
 	return s.Interface(), nil
+}
+
+func toBareStructValue(obj interface{}) (reflect.Value, error) {
+	v := reflect.ValueOf(obj)
+	if v.Kind() != reflect.Ptr {
+		return reflect.Value{}, errors.New("must be a pointer")
+	}
+	if v.IsNil() {
+		return reflect.Value{}, errors.New("must be a non-nil pointer")
+	}
+	v = v.Elem()
+	if v.Kind() != reflect.Struct {
+		return reflect.Value{}, errors.New("must be a pointer to struct")
+	}
+
+	return v, nil
 }
